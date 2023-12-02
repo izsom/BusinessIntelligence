eb43af1720e68822dfc9eee20f60a62407bab000
will amminger <117075727+a350-900ulr@users.noreply.github.com>
2023-12-02 15:55:27 +0100

    added both paths in parameters,

--- Sub2/BI_Projects/BI_60/task2/etl_sales/Dim_Product_Fill.sql
+++ Sub2/BI_Projects/BI_60/task2/etl_sales/Dim_Product_Fill.sql
@@ -1,33 +1,32 @@
 -- Populate Dim_Product Table
 INSERT INTO Dim_Product (
-    ProductKey,
-    ProductName,
-    ProductModelName,
-    ProductSubCategoryName,
-    ProductTopCategoryName,
-    StandardCost,
-    ListPrice,
-    StartDate,
-    EndDate,
-    ProductStatus
-)
-SELECT
-    p.ProductID AS ProductKey,
-    p.ProductName as ProductName,
-    pm.ProductModelName as ProductModelName,
-    psc.Name AS ProductSubCategoryName,
-    ptc.Name AS ProductTopCategoryName,
-    p.StandardCost as StandardCost,
-    p.ListPrice as ListPrice,
-    p.SellStartDate as StartDate,
-    p.SellEndDate as EndDate,
-    CASE 
-        WHEN p.SellEndDate IS NULL OR p.SellEndDate > '2021-09-30' THEN 'Current' ELSE 'Discontinued' END AS ProductStatus
-FROM
-    TB_Product p
-JOIN
-    TB_ProductModel pm ON p.ProductModelID = pm.ProductModelID
-JOIN
-    TB_ProductSubCategory psc ON p.ProductSubCategoryID = psc.ProductSubCategoryID
-JOIN
-    TB_ProductTopCategory ptc ON psc.ProductTopCategoryID = ptc.ProductTopCategoryID;
+	ProductKey,
+	ProductName,
+	ProductModelName,
+	ProductSubCategoryName,
+	ProductTopCategoryName,
+	StandardCost,
+	ListPrice,
+	StartDate,
+	EndDate,
+	ProductStatus
+) SELECT
+	p.ProductID         AS ProductKey,
+	p.ProductName       AS ProductName,
+	pm.ProductModelName AS ProductModelName,
+	psc.Name            AS ProductSubCategoryName,
+	ptc.Name            AS ProductTopCategoryName,
+	p.StandardCost      AS StandardCost,
+	p.ListPrice         AS ListPrice,
+	p.SellStartDate     AS StartDate,
+	p.SellEndDate       AS EndDate,
+	IF(p.SellEndDate IS NULL OR p.SellEndDate > '2021-09-30', 'Current', 'Discontinued')
+		AS ProductStatus
+
+FROM TB_Product p
+	JOIN
+		TB_ProductModel pm ON p.ProductModelID = pm.ProductModelID
+	JOIN
+		TB_ProductSubCategory psc ON p.ProductSubCategoryID = psc.ProductSubCategoryID
+	JOIN
+		TB_ProductTopCategory ptc ON psc.ProductTopCategoryID = ptc.ProductTopCategoryID;

f1cb8e7563e85413db2c9d7cab12d0fe3b7cd700
will amminger <117075727+a350-900ulr@users.noreply.github.com>
2023-12-05 17:51:32 +0100

    fixed 3.3

--- Sub2/BI_Projects/BI_60/task3/3_5.sql
+++ Sub2/BI_Projects/BI_60/task3/3_5.sql
@@ -1,20 +1,20 @@
 SELECT
-    dl.Country,
-    dd.CalendarYear AS YearShipment,
-    dd.EnglishMonthName AS MonthShipment,
-    SUM(fis.OrderLineShippingCost) AS ShippingCosts
+	dl.Country,
+	dd.CalendarYear AS Year,
+	dd.EnglishMonthName AS Month,
+	SUM(fis.OrderLineShippingCost) AS ShippingCosts
 FROM
-    Fact_InternetSales fis
-JOIN
-    Dim_Location dl ON fis.ShipToLocationKey = dl.LocationKey
-JOIN
-    Dim_Date dd on dd.DateKey = fis.OrderDateKey
+	Fact_InternetSales fis
+		JOIN
+	Dim_Location dl ON fis.ShipToLocationKey = dl.LocationKey
+		JOIN
+	Dim_Date dd on dd.DateKey = fis.OrderDateKey
 WHERE
-    dl.Country = 'United Kingdom'
-    AND fis.ShipMethod = 'Cargo International'
-    AND dd.CalendarYear = 2020
-    AND dd.MonthNumberOfYear BETWEEN 1 AND 6
+	dl.Country = 'United Kingdom'
+	AND fis.ShipMethod = 'Cargo International'
+	AND dd.CalendarYear = 2020
+	AND dd.MonthNumberOfYear BETWEEN 1 AND 6
 GROUP BY
-    dl.Country,
-    YearShipment,
-    MonthShipment
+	dl.Country,
+	Year,
+	Month
